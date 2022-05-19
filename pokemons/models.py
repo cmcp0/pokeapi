231b438c671cbdd535f568f2fec2df11a6b81837
Carlos Caceres <cmcacerespalacio@gmail.com>
2022-05-19 06:59:05 -0500

    add models

--- pokemons/models.py
+++ pokemons/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    id = models.IntegerField()
+    name = models.CharField()
+    height = models.DecimalField(decimal_places=1)
+    weight = models.DecimalField(decimal_places=1)
+
+class EvolutionType(models.Model):
+    pokemon = models.ForeignKey(Pokemon)
+
+    class Meta:
+        abstract = True
+
+class Evolution(EvolutionType):
+    pass
+
+class PreEvolution(EvolutionType):
+    pass
+
+class BaseStat(models.Model):
+    pokemon = models.ForeignKey(Pokemon)
+    name = models.CharField()
+    stat = models.IntegerField()
+    effort = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class Hp(BaseStat):
+    pass
+
+class Attack(BaseStat):
+    pass
+
+class Defense(BaseStat):
+    pass
+class SpecialAttack(BaseStat):
+    pass
+
+class SpecialDefense(BaseStat):
+    pass
+
+class Speed(BaseStat):
+    pass
