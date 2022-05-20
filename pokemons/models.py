c70a88347dccec86c1d359d9d89eed62a4605454
Carlos Caceres <cmcacerespalacio@gmail.com>
2022-05-20 00:10:58 -0500

    fix models fields

--- pokemons/models.py
+++ pokemons/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 class Pokemon(models.Model):
-    id = models.IntegerField()
-    name = models.CharField()
-    height = models.DecimalField(decimal_places=1)
-    weight = models.DecimalField(decimal_places=1)
+    id = models.IntegerField(primary_key= True)
+    name = models.CharField(max_length= 50)
+    height = models.DecimalField(decimal_places=1, max_digits= 6)
+    weight = models.DecimalField(decimal_places=1, max_digits= 6)
+
+    def __str__(self) -> str:
+        return self.name
 
 class EvolutionType(models.Model):
-    pokemon = models.ForeignKey(Pokemon)
+    key = models.ForeignKey(Pokemon, on_delete= models.CASCADE)
+    id_pokemon = models.IntegerField()
+    name_pokemon = models.CharField(max_length= 50)
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return self.key.name    
 
 class Evolution(EvolutionType):
     pass
@@ -19,8 +27,8 @@ class PreEvolution(EvolutionType):
     pass
 
 class BaseStat(models.Model):
-    pokemon = models.ForeignKey(Pokemon)
-    name = models.CharField()
+    pokemon = models.ForeignKey(Pokemon, on_delete= models.CASCADE)
+    name = models.CharField(max_length= 50)
     stat = models.IntegerField()
     effort = models.IntegerField()
 
